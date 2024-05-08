ce73985b4d8606ab01c08343e3403673cacc6e3a
NautroKazenaki <kalliak2014@yandex.ru>
2024-05-08 17:31:34 +0300

    first req+res try

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -4,46 +4,62 @@ import LPStyles from './LoginPage.module.css';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = ({ setIsLoggedIn }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
-    // const getUsersAndAuthenticate = async (e, email, password) => {
-    //     e.preventDefault();
-    //     try {
-    //         const users = await window.api.getAllUsers();
-    //         const user = users.find(u => u.name === email && u.password === password);
-
-    //         if (user) {
-    //             const { name, password, level } = user;
-
-    //             // Authentication successful
-    //             toast.success('Вы успешно вошли!');
-    //             localStorage.setItem("user", JSON.stringify({ email, password, level }));
-    //             localStorage.setItem('isLoggedIn', 'true');
-    //             setIsLoggedIn(true);
-    //             setEmail("");
-    //             setPassword("");
-    //             navigate("/acceptance-page");
-    //         } else {
-    //             // Authentication failed
-    //             toast.error('Неверный email или пароль!');
-    //         }
-    //     } catch (error) {
-    //         console.error('Ошибка при аутентификации пользователя:', error);
-    //         throw error; // Rethrow the error for handling elsewhere
-    //     }
-    // };
+    const getUsersAndAuthenticate = async (e, email, password) => {
+        e.preventDefault();
+        try {
+            if (email !== '' && password !== '') {
+                fetch('http://192.168.0.139:3001/users', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        name: email,
+                        password: password
+                    })
+                } ).then(response => {
+                    if (!response.ok) {
+                      throw new Error('Network response was not ok');
+                    }
+                    return response.body; // Parse response body as JSON
+                  })
+                  .then(data => {
+                    console.log(data); // Handle response data
+                  })
+                // axios.post('http://192.168.0.139:3001/users', { email, password }).then(
+                //     res => {
+                //         console.log(res.data)
+                //     }
+                // )
+                // // Authentication successful
+                // toast.success('Вы успешно вошли!');
+                // // localStorage.setItem("user", JSON.stringify({ email, password, level }));
+                // // localStorage.setItem('isLoggedIn', 'true');
+                // setIsLoggedIn(true);
+                // setEmail("");
+                // setPassword("");
+                // navigate("/acceptance-page");
+            } else {
+                // Authentication failed
+                toast.error('Неверный email или пароль!');
+            }
+        } catch (error) {
+            console.error('Ошибка при аутентификации пользователя:', error);
+            throw error; // Rethrow the error for handling elsewhere
+        }
+    };
 
     return (
         <>
             <div className={LPStyles.container}>
                 <div className={LPStyles.screen}>
                     <div className={LPStyles.screen__content}>
-                        <form className={LPStyles.login} onSubmit={(e) => alert('evrything is fine')}>
-                        {/* <form className={LPStyles.login} onSubmit={(e) => getUsersAndAuthenticate(e, email, password)}> */}
+                        {/* <form className={LPStyles.login} onSubmit={(e) => alert('evrything is fine')}> */}
+                         <form className={LPStyles.login} onSubmit={(e) => getUsersAndAuthenticate(e, email, password)}>
                             <div className={LPStyles.login__field}>
                                 <input type="text" className={LPStyles.login__input} placeholder="ВВЕДИТЕ ФИО" onChange={(e) => setEmail(e.target.value)} />
                             </div>
